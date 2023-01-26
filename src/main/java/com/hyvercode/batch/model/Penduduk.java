0e1e58823db4011418ea3b35f59d754575d9d2dc
mohirwanh <mohirwanh@gmail.com>
2023-01-26 14:28:53 +0700

    Init

--- src/main/java/com/hyvercode/batch/model/Penduduk.java
+++ src/main/java/com/hyvercode/batch/model/Penduduk.java
@@ -1,11 +1,13 @@
 package com.hyvercode.batch.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Builder
 @Entity
